13c7ee7b439d63cdde8d7826a19b6c2b74352e56
SovaVika <104214669+SovaVika@users.noreply.github.com>
2024-03-30 11:12:23 +0200

    tiktaktoe.py

--- tiktaktoe.py
+++ tiktaktoe.py
@@ -7,6 +7,11 @@ win = 0
 for i in range(2):
     player = input("Додати гравця - ")
     pl.append(player)
+
+f = open("test.txt", "w")
+f.write(f"\nОшибки - ")
+f.close() 
+
 qu = 1
 no_game = 0
 is_game = 0
@@ -40,76 +45,84 @@ while win == 0 and is_game == 0 and no_game == 0:
             else:
                 no_game = 1
 
-
-    if gm[0] == X and gm[1] == X and gm[2] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[1]} переміг(ла)")
-        win = 1
-    elif gm[3] == X and gm[4] == X and gm[5] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[1]} переміг(ла)")
-        win = 1
-    elif gm[6] == X and gm[7] == X and gm[8] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[1]} переміг(ла)")
-        win = 1
-    elif gm[0] == X and gm[4] == X and gm[8] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[1]} переміг(ла)")
-        win = 1
-    elif gm[6] == X and gm[4] == X and gm[2] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[1]} переміг(ла)")
-        win = 1
-    elif gm[0] == X and gm[3] == X and gm[6] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[1]} переміг(ла)")
-        win = 1
-    elif gm[1] == X and gm[4] == X and gm[7] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[1]} переміг(ла)")
-        win = 1
-    elif gm[2] == X and gm[5] == X and gm[8] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[1]} переміг(ла)")
-        win = 1
-
-
+    try:
+        if gm[0] == X and gm[1] == X and gm[2] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[1]} переміг(ла)")
+            win = 1
+        elif gm[3] == X and gm[4] == X and gm[5] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[1]} переміг(ла)")
+            win = 1
+        elif gm[6] == X and gm[7] == X and gm[8] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[1]} переміг(ла)")
+            win = 1
+        elif gm[0] == X and gm[4] == X and gm[8] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[1]} переміг(ла)")
+            win = 1
+        elif gm[6] == X and gm[4] == X and gm[2] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[1]} переміг(ла)")
+            win = 1
+        elif gm[0] == X and gm[3] == X and gm[6] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[1]} переміг(ла)")
+            win = 1
+        elif gm[1] == X and gm[4] == X and gm[7] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[1]} переміг(ла)")
+            win = 1
+        elif gm[2] == X and gm[5] == X and gm[8] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[1]} переміг(ла)")
+            win = 1
 
 
 
-    if gm[0] == O and gm[1] == O and gm[2] == O:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[0]} переміг(ла)")
-        win = 1
-    elif gm[3] == O and gm[4] == O and gm[5] == O:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[0]} переміг(ла)")
-        win = 1
-    elif gm[6] == O and gm[7] == O and gm[8] == O:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[0]} переміг(ла)")
-        win = 1
-    elif gm[0] == O and gm[4] == O and gm[8] == O:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[0]} переміг(ла)")
-        win = 1
-    elif gm[6] == O and gm[4] == O and gm[2] == O:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[0]} переміг(ла)")
-        win = 1
-    elif gm[0] == X and gm[3] == X and gm[6] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[0]} переміг(ла)")
-        win = 1
-    elif gm[1] == X and gm[4] == X and gm[7] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[0]} переміг(ла)")
-        win = 1
-    elif gm[2] == X and gm[5] == X and gm[8] == X:
-        print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
-        print(f"{pl[0]} переміг(ла)")
-        win = 1
+
+
+        if gm[0] == O and gm[1] == O and gm[2] == O:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[0]} переміг(ла)")
+            win = 1
+        elif gm[3] == O and gm[4] == O and gm[5] == O:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[0]} переміг(ла)")
+            win = 1
+        elif gm[6] == O and gm[7] == O and gm[8] == O:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[0]} переміг(ла)")
+            win = 1
+        elif gm[0] == O and gm[4] == O and gm[8] == O:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[0]} переміг(ла)")
+            win = 1
+        elif gm[6] == O and gm[4] == O and gm[2] == O:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[0]} переміг(ла)")
+            win = 1
+        elif gm[0] == X and gm[3] == X and gm[6] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[0]} переміг(ла)")
+            win = 1
+        elif gm[1] == X and gm[4] == X and gm[7] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[0]} переміг(ла)")
+            win = 1
+        elif gm[2] == X and gm[5] == X and gm[8] == X:
+            print(f"\n|     |     |     |\n|  {gm[0]}  |  {gm[1]}  |  {gm[2]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[3]}  |  {gm[4]}  |  {gm[5]}  |\n|_____|_____|_____|\n|     |     |     |\n|  {gm[6]}  |  {gm[7]}  |  {gm[8]}  |\n|     |     |     |\n")
+            print(f"{pl[0]} переміг(ла)")
+            win = 1
+    except NameError:
+        f = open("test.txt", "a") 
+        f.write(f"\nNameError")
+        f.close()
+    except ZeroDivisionError:
+        f = open("test.txt", "a") 
+        f.write(f"\nZeroDivisionError")
+        f.close()
 
 
 
@@ -134,10 +147,17 @@ while win == 0 and is_game == 0 and no_game == 0:
 
     if is_game1 == 1 and is_game2 == 1 and is_game3 == 1 and is_game4 == 1 and is_game5 == 1 and is_game6 == 1 and is_game7 == 1 and is_game8 == 1 and is_game9 == 1:
         is_game = 1
-
-if is_game == 1:
-    print("")
-    print("Нічия")
-if no_game == 1:
-    print("")
-    print("Неправильний хід, кінець гри")
+class GAME:
+    def __init__(self, is_game, no_game):
+        self.is_game = is_game
+        self.no_game = no_game
+    def __str__(self):
+        if is_game == 1:
+            return(f"\nНічия")
+        elif no_game == 1:
+            return(f"\nНеправильний хід, кінець гри")
+        else:
+            return(f"")
+game = GAME(is_game, no_game)
+print(game)
+    
